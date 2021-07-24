713ffb060b63e7a59cf5460c70649b43cc54fb26
jung0930 <wjddlqkqh57@naver.com>
2021-07-24 11:28:39 +0900

    refactor : RacingApp을 컨트롤러로 변경

--- src/main/java/racingCar/RacingApp.java
+++ src/main/java/racingCar/RacingApp.java
@@ -1,12 +1,45 @@
 package racingCar;
 
-import racingCar.controller.RacingController;
+import racingCar.domain.Car;
+import racingCar.domain.Race;
+import racingCar.view.InputView;
+import racingCar.view.ResultView;
+
+import java.util.List;
 
 public class RacingApp {
 
+    InputView inputView = new InputView();
+    ResultView resultView = new ResultView();
+
     public static void main(String[] args) {
-        RacingController controller = new RacingController();
-        controller.Race();
+        RacingApp app = new RacingApp();
+        app.Race();
+    }
+
+    private void Race() {
+        int carCount = this.inputView.EnterCarNumber();
+        int moveCount = this.inputView.EnterMoveCount();
+
+        Race race = new Race(carCount);
+
+        start(race, moveCount, carCount);
+    }
+
+    private void start(Race race, int moveCount, int carCount) {
+        this.resultView.printExecutionResultMessage();
+
+        for(int i = 0; i < moveCount; i++) {
+            race = race.moveCar();
+            outputCarDistance(race.getCars(), carCount);
+        }
+    }
+
+    private void outputCarDistance(List<Car> cars, int carCount) {
+        for(int i = 0; i < carCount; i++) {
+            this.resultView.printCarDistance(cars.get(i).getCarDistance());
+        }
+        this.resultView.printEnter();
     }
 
 }
